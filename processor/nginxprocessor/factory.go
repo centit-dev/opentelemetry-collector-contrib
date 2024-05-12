b5318464c0973615be48f94f8a03484daf69d479
teanoon <anticipationvu@gmail.com>
2024-05-12 22:03:29 +0800

    feat(nginxprocessor): add nginx processor

---
+++ processor/nginxprocessor/factory.go
@@ -0,0 +1,37 @@
+package nginxprocessor
+
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/processor"
+	"go.opentelemetry.io/collector/processor/processorhelper"
+)
+
+var (
+	Type = component.MustNewType("nginxprocessor")
+)
+
+func NewFactory() processor.Factory {
+	return processor.NewFactory(
+		Type,
+		createDefaultConfig,
+		processor.WithTraces(createTraceProcessor, component.StabilityLevelAlpha),
+	)
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{}
+}
+
+func createTraceProcessor(ctx context.Context, params processor.CreateSettings, cfg component.Config, nextConsumer consumer.Traces) (processor.Traces, error) {
+	processor := CreateTraceProcessor()
+	// create trace processor with process func
+	return processorhelper.NewTracesProcessor(
+		ctx,
+		params,
+		cfg,
+		nextConsumer,
+		processor.ProcessTraces)
+}
